fa27b06b839477b2c83af28bb1317a068308a12e
Kim-Shinmi <cheri0901@gmail.com>
2024-01-09 20:46:21 +0900

    [1/9] 웹페이지 최종 수정본

--- single_pages/urls.py
+++ single_pages/urls.py
@@ -3,10 +3,14 @@ from . import views
 
 urlpatterns = [
     path("", views.index),
-    path("jobList/", views.jobList),
-    path("jobInterest/", views.jobInterest),
-    path("jobExperience/", views.jobExperience),
-    path("futureJob/", views.futureJob),
-    path("futureJobSkill/", views.futureJobSkill),
-    path("futureJobMedia/", views.futureJobMedia),
+    path("jobList", views.jobList),
+    path("jobInterest", views.jobInterest),
+    path("jobExperience", views.jobExperience, name='news_search'),
+    path("futureJob", views.futureJob),
+    path("futureJobSkill", views.futureJobSkill),
+    path("futureJobMedia", views.futureJobMedia),
+    path("jobex", views.jobex),
+    path("joby", views.joby),
+    path("game1", views.game1),
+    path("game2", views.game2),
 ]
