7ad2c1e973e78627e1b69e01288607b257a12c2a
Kim-Shinmi <cheri0901@gmail.com>
2024-01-09 11:46:38 +0900

    정리용 폴더 삭제

--- 프로젝트 공유용/[정리용] 추가／수정 파일들/board/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import Post            # Post 모델을 불러옴
-
-# Register your models here.
-admin.site.register(Post)           # Post 모델을 관리자 페이지에서 볼 수 있도록 등록
--- 프로젝트 공유용/[정리용] 추가／수정 파일들/board/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('<int:pk>/', views.boardPage),         # <int:pk> ::: pk가 정수형임을 명시
-    path('', views.boardList),                             # blog 앱에서의 기본(메인) 페이지
-
-]
-
